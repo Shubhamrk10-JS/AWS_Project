de2c8c5e64ae4e739e5fe1b7e4955e0408959fce
Shubhamrk10-JS <shubhamrk999@gmail.com>
2025-10-02 21:36:33 +0530

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,13 @@
+myRegion = "us-west-1"
+MyVPC = "Main_VPC"
+VPCCIDR  = "10.10.0.0/16"
+
+Availability_zone_1a = "us-west-1a"
+public_subnet_1a_cidr = "10.10.1.0/24"
+private_subnet_1a_cidr = "10.10.2.0/24"
+
+
+Availability_zone_1c = "us-west-1c"
+public_subnet_1c_cidr = "10.10.3.0/24"
+private_subnet_1c_cidr = "10.10.4.0/24"
+
---
+++ zone_1a.tf
@@ -0,0 +1,27 @@
+resource "aws_subnet" "Pub_Subnet_1a" {
+  
+  vpc_id = aws_vpc.MyVPC.id
+
+  availability_zone = var.Availability_zone_1a
+
+  cidr_block = var.public_subnet_1a_cidr
+
+  tags = {
+    Name = "Public Subnet 1a"  }
+}
+
+resource "aws_subnet" "Pvt_Subnet_1a" {
+  
+  vpc_id = aws_vpc.MyVPC.id
+
+  availability_zone = var.Availability_zone_1a
+
+  cidr_block = var.private_subnet_1a_cidr
+
+  tags = {
+    Name = "Private Subnet 1a"  }
+}
+
+variable "Availability_zone_1a" {}
+variable "public_subnet_1a_cidr" {}
+variable "private_subnet_1a_cidr" {}
